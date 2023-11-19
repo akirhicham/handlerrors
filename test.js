2210df15acf8386eebc8220aadd8012895b8d8b7
armahicham <hicham.akir@arma.ma>
2023-11-19 17:44:56 +0100

    final vers

--- test.js
+++ test.js
@@ -1,17 +1,19 @@
+// This code will guide you on how this package works.
+
 const QueryHandler = require('./handleerrors');
 //Database Config
 const dbConfig = {
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'armas_kpi',//Database name
+    database: 'database_name',//Database name
 };
 
 const queryHandler = new QueryHandler(dbConfig);
 //Query to be executed (example:Delete by ID)
-const sampleQuery = 'Delete from Indicators where IndicatorID=?';
+const sampleQuery = 'Delete from table where ID=?';
 
-const queryParams =[17];
+const queryParams =[17];//parameter
 queryHandler.executeQuery(sampleQuery, queryParams)
     .then((message) => {
         console.log(message);
